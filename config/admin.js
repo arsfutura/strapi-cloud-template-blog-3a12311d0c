9b01e01d9058e160a8331e344d75152b90b33ccb
Filip Bolčić <filip.bolcic@arsfutura.co>
2025-02-18 16:27:07 +0100

    feat: add preview feature

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,64 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+module.exports = ({ env }) => {
+  const clientUrl = env("CLIENT_URL"); // Frontend application URL
+  const previewSecret = env("PREVIEW_SECRET");
+
+  return {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
     },
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
-});
+    apiToken: {
+      salt: env("API_TOKEN_SALT"),
+    },
+    transfer: {
+      token: {
+        salt: env("TRANSFER_TOKEN_SALT"),
+      },
+    },
+    flags: {
+      nps: env.bool("FLAG_NPS", true),
+      promoteEE: env.bool("FLAG_PROMOTE_EE", true),
+    },
+    preview: {
+      enabled: true,
+      config: {
+        allowedOrigins: clientUrl,
+        async handler(uid, { documentId, locale, status }) {
+          // Fetch the complete document from Strapi
+          const document = await strapi.documents(uid).findOne({ documentId });
+
+          // Generate the preview pathname based on content type and document
+          const pathname = getPreviewPathname(uid, { locale, document });
+
+          // Disable preview if the pathname is not found
+          if (!pathname) {
+            return null;
+          }
+
+          // Use Next.js draft mode passing it a secret key and the content-type status
+          const urlSearchParams = new URLSearchParams({
+            url: pathname,
+            secret: previewSecret,
+            status,
+          });
+          return `${clientUrl}/api/preview?${urlSearchParams}`;
+        },
+      },
+    },
+  };
+};
+
+const getPreviewPathname = (uid, { locale, document }) => {
+  const { slug } = document;
+
+  switch (uid) {
+    case "api::article.article": {
+      if (!slug) {
+        return null;
+      }
+      return `/articles/${slug}`;
+    }
+    default: {
+      return null;
+    }
+  }
+};
